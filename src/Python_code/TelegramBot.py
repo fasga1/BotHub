47c4df4ca0bb68a5540560ac0ef4144bd44ce215
Potap Eremin <potikeremin@gmail.com>
2025-10-16 18:08:42 +0300

    develop/Potap_branch Added a welcome email, a "Register" button, and protected the bot token.

--- src/Python_code/TelegramBot.py
+++ src/Python_code/TelegramBot.py
@@ -1,20 +1,38 @@
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+import os
+from dotenv import load_dotenv
 
-TOKEN = "8173274497:AAFQppVLPx3LgJi1GdaQHgVOZwj6n8aFTFQ"
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+Button_text = "Зарегистрироваться"
 
 async def start(update, context):
-    await update.message.reply_text('Привет! Я твой бот 😊')
+    button = KeyboardButton(Button_text)
+    keyboard = [[button]]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=False)
 
-async def echo(update, context):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text(
+    "Привет!\nВы запустили WB_Congratulations_bot\n\nВот инструкция по пользованию ботом для вас:\n\nПервым делом, для работы в боте вам необходимо зарегистрироваться используя вашу корпаративную почту и выданый вам пароль, для этого нажмите на кнопку 'Зарегистрироваться'",
+        reply_markup=reply_markup
+    )
+
+async def handle_button(update, context):
+    user_text = update.message.text
+    if user_text == Button_text:
+        await update.message.reply_text("Введите ваш ЛОГИН и ПАРОЛЬ в формате:\n ЛОГИН:{ваша почта}\n ПАРОЛЬ:{ваш пароль}")
+    else:
+        await update.message.reply_text("Я понимаю только кнопку!")
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_button))
 
-    print("Первоначальная версия бота")
+    print("Бот запущен!")
     app.run_polling()
 
 if __name__ == '__main__':
